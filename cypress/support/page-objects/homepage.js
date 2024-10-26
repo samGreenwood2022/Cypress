6dbf22fdc48782eead7b809a34bb5fa8ebe1aa16
SamGreenwood <sam_greenwood26@hotmail.com>
2024-10-26 14:04:01 +0100

    re-added the search method

--- cypress/support/page-objects/homepage.js
+++ cypress/support/page-objects/homepage.js
@@ -4,8 +4,13 @@ class HomePage {
       searchInput: () => cy.get('[data-cy="searchFieldSearch"]')
                             .first(),
     };
-  
-    
+
+    enterSearchTerm(searchTerm) {
+      console.log(searchTerm); // Debugging step
+      this.elements.searchInput().type(searchTerm);
+    }
+
   }
+  
   
   export default new HomePage();
