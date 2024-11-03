ffc138042021929dcd2472f084adeef44f73b9ad
SamGreenwood <sam_greenwood26@hotmail.com>
2024-11-03 10:44:18 +0000

    amended cypress.config

--- cypress.config.js
+++ cypress.config.js
@@ -1,28 +1,26 @@
-const { defineConfig } = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-preprocessor");
 const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
-module.exports = defineConfig({
+module.exports = {
   e2e: {
     specPattern: [
       'cypress/e2e/**/*.feature', // Use '**' to match subdirectories
       'cypress/e2e/0-test/*.js' // Same for step definitions
-    ],    
+    ],
     //stepDefinitions: "cypress/support/step_definitions/*.js",
-    async setupNodeEvents(on, config) {
-      // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
-      await addCucumberPreprocessorPlugin(on, config);
-
-      on(
-        "file:preprocessor",
-        createBundler({
-          plugins: [createEsbuildPlugin(config)],
-        })
-      );
-
-      // Make sure to return the config object as it might have been modified by the plugin.
-      return config;
+    setupNodeEvents(on, config) {
+      // This is required for the preprocessor to be able to generate JSON reports after each run, and more
+      return addCucumberPreprocessorPlugin(on, config).then(() => {
+        on(
+          "file:preprocessor",
+          createBundler({
+            plugins: [createEsbuildPlugin(config)],
+          })
+        );
+        // Make sure to return the config object as it might have been modified by the plugin.
+        return config;
+      });
     },
   },
-});
+};
