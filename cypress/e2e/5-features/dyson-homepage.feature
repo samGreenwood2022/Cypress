46a08118c48006d30365c93142dc046ffe4dfd77
SamGreenwood <sam_greenwood26@hotmail.com>
2025-01-05 09:43:15 +0000

    fixed link

--- cypress/e2e/5-features/dyson-homepage.feature
+++ cypress/e2e/5-features/dyson-homepage.feature
@@ -2,20 +2,25 @@ Feature: Dyson Homepage Regression Tests
 
   Background: I visit the manufacturer home page
     Given I visit the manufacturer home page
-   
 
   Scenario Outline: Verify URL contains expected text
     Then The URL will contain the expected text "<expectedText>"
 
-  Examples:
-    | expectedText           |
-    | /manufacturer/dyson/   |
-    | /overview              |
+    Examples:
+      | expectedText           |
+      | /manufacturer/dyson/   |
+      | /overview              |
 
+  Scenario Outline: I verify the telephone link attribute
+    Then The number will be correct, the href will be as expected, and the telephone protocol will correct "<telNo>"
 
-  Scenario: Verify telephone link attribute
-    Then The number will be correct and the href will be as expected "<numberHref>"
+    Examples:
+      | numberHref        | 
+      | tel:08003457788   | 
 
-  Examples:
-    | numberHref        | 
-    | tel:08003457788   | 
+  Scenario Outline: Verify the h1 title text on page
+    Then The h1 title text will be as expected "<h1Text>"
+
+    Examples:
+      | h1Text        | 
+      | Dyson         |
--- cypress/support/step_definitions/dyson-homepage.js
+++ cypress/support/step_definitions/dyson-homepage.js
@@ -1,33 +1,36 @@
-const HomePage = require("../../support/page-objects/homepage");
-const BasePage = require("../../support/page-objects/base-page");
-const ManufacturerHomePage = require("../../support/page-objects/manufacturer-homepage");
-const { verifyH1Text, verifyUrlContents } = require('../page-objects/manufacturer-homepage');
+const HomePage = require("../../page-objects/homepage");
+const BasePage = require("../../page-objects/base-page");
+const ManufacturerHomePage = require("../../page-objects/manufacturer-homepage");
 
 var { Given, Then } = require('@badeball/cypress-cucumber-preprocessor');
 
-
 const baseURL = "https://source.thenbs.com/";
 const basePage = new BasePage(baseURL);
-
+const manufacturerHomePage = new ManufacturerHomePage();
 
 beforeEach(() => {
-    basePage.visit(); // Visit the base URL
-    HomePage.acceptCookies(); // Accept cookies
-    HomePage.enterSearchTerm("Dyson"); // Enter search term
-  })
+  basePage.visit(); // Visit the base URL
+  HomePage.acceptCookies(); // Accept cookies
+  HomePage.enterSearchTerm("Dyson"); // Enter search term
+});
 
 Given(`I visit the manufacturer home page`, () => {
-    verifyH1Text('Dyson')
+  manufacturerHomePage.verifyH1Text('Dyson');
 });
 
-Then('The URL will contain the expected text {string}', (expectedText) => {
-    cy.url().should('include', expectedText);
-  });
-
-Given(`I click the telephone number link`, (hrefText) => {
-    ManufacturerHomePage.verifyLinkHref(hrefText)
+Then(`The URL will contain the expected text {string}`, (expectedText) => {
+  cy.url().should('include', expectedText);
 });
 
-Then('The number will be correct and the href will be as expected {string}', (telNo) => {
-    ManufacturerHomePage.verifyTelephoneLinkAttribute(telNo)
-  });
+Then(`The number will be correct, the href will be as expected, and the telephone protocol will correct {string}`, (telNo) => {
+  manufacturerHomePage.verifyTelephoneLinkAttribute(telNo);
+});
+
+Then(`The h1 title text will be as expected {string}`, (h1Text) => {
+  manufacturerHomePage.verifyH1Text(h1Text);
+});
+
+
+
+
+
