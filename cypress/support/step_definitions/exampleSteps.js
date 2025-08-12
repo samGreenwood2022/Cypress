91a605623b7851fae07f424ef0e22b1eb16b8ef5
samGreenwood2022 <120378330+samGreenwood2022@users.noreply.github.com>
2025-08-12 15:10:51 +0100

    Removed some unused files

--- cypress/support/step_definitions/exampleSteps.js
+++
@@ -1,9 +0,0 @@
-const { Given, Then } = require("@badeball/cypress-cucumber-preprocessor");
-
-Given("I visit the home page", () => {
-  cy.visit("https://example.com");
-});
-
-Then("I should see the welcome message", () => {
-  cy.contains("Example Domain");
-});
