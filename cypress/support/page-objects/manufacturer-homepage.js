8fb3e14ee25f57b290d44db61f745b91c80ba372
samGreenwood2022 <120378330+samGreenwood2022@users.noreply.github.com>
2025-09-02 09:27:23 +0100

    Set viewport size for UI and API content verification

--- cypress/support/page-objects/manufacturer-homepage.js
+++ cypress/support/page-objects/manufacturer-homepage.js
@@ -127,6 +127,7 @@ class ManufacturerHomePage extends BasePage {
   }
 
   verifyUIandAPIContent() {
+    cy.viewport(1000, 4410)
     cy.request({
       method: "GET",
       url: "https://geolocation.onetrust.com/cookieconsentpub/v1/geo/location",
