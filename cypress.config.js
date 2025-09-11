588ab0fac0ec4e6810a7da5264b4e1ce13daccff
samGreenwood2022 <120378330+samGreenwood2022@users.noreply.github.com>
2025-09-11 09:49:08 +0100

    refactor: improve comments in Cypress configuration for clarity

--- cypress.config.js
+++ cypress.config.js
@@ -9,9 +9,7 @@ const path = require('path');
 module.exports = defineConfig({
   projectId: "4afu4o", // Add this line for Cypress Cloud integration
   e2e: {
-    experimentalStudio: true,  // Add this line
-    // viewportWidth: 990,
-    // viewportHeight: 800,
+    experimentalStudio: true,  // This will enabled the Cypress Studio feature
     specPattern: [
       'cypress/e2e/5-features/*.feature', // Define path to feature files
       'cypress/e2e/1-getting-started/*.js', // Define path to feature files
