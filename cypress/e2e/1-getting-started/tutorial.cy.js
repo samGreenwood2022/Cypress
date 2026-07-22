1c4a2ac9add92331d7b04596f097ae036960505f
Sam Greenwood <sam_greenwood26@hotmail.com>
2026-07-22 16:13:59 +0100

    feat: improve test structure and assertions for Dyson homepage navigation

--- cypress/e2e/1-getting-started/tutorial.cy.js
+++ cypress/e2e/1-getting-started/tutorial.cy.js
@@ -1,10 +1,23 @@
-describe('NBS Regression Tests', () => {
-  it('navigate to dyson manufacturer homepage', () => {
-    cy.visit('https://source.thenbs.com/en/gb')
+describe("NBS Regression Tests", () => {
+  it("navigate to dyson manufacturer homepage", () => {
+    cy.visit("https://source.thenbs.com/en/gb");
+    cy.get('[data-cy="searchFieldSearch"]').first().type("dyson{enter}");
+    cy.get('[data-cy="tabCategory"]').contains("Manufacturers").click();
+    cy.get('a[title="View Dyson"]').click()
+    cy.url().should("include", "manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview");
+  });
 
-    //cy.get('[data-cy="searchFieldSearch"]').should()
-    
+  it("assert that the h1 contains 'Dyson'", () => {
+    cy.visit("https://source.thenbs.com/en/gb");
+    cy.get('[data-cy="searchFieldSearch"]').first().type("dyson{enter}");
+    cy.get('[data-cy="tabCategory"]').contains("Manufacturers").click();
+    cy.get('a[title="View Dyson"]').click()
+    cy.url().should("include", "manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview");
+    cy.get('h1').should('contain.text', 'Dyson');
+  });
+
+  
 
 
-  })
-})
+
+});
